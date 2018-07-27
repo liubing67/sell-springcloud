539364f1e1aea141bef69fcd59bfd07c4186ee19
liubing67 <724266839@qq.com>
2018-07-27 14:47:03 +0800

    配置更改

--- eureka-server/src/main/java/com/abing/eurekaserver/EurekaApplication.java
+++ eureka-server/src/main/java/com/abing/eurekaserver/EurekaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableEurekaServer
 public class EurekaApplication {
 
